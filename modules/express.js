fb3aaa8e7780d8a1379a3d1eda7ad6358deff827
Guviega <guviega@gmail.com>
2026-03-26 17:56:17 -0300

    LearningNode: CRUD using EXPRESS and MONGOOSE

--- modules/express.js
+++ modules/express.js
@@ -1,6 +1,8 @@
 const express = require(`express`);
+const UserModel = require(`../src/models/user.model`);
 
 const app = express();
+app.use(express.json());
 
 app.get(`/home`, (req, res) => {
   res.status(200).send(`Hello Express World`);
@@ -14,13 +16,62 @@ const users = [
   { name: `Mabel`, email: `melbf@gmail.com` },
 ];
 
-app.get(`/users`, (req, res) => {
-  res.contentType(`application/html`);
-  res.status(200).json(users);
+//Find all Users
+app.get(`/users`, async (req, res) => {
+  try {
+    const users = await UserModel.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
+// Find User by ID
+app.get(`/users/:id`, async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await UserModel.findById(id);
+
+    res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
+// Update User by ID
+app.patch(`/users/:id`, async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const user = await UserModel.findByIdAndUpdate(id, req.body, { new: true });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.delete(`/users/:id`, async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await UserModel.findByIdAndDelete(id);
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+// Create User
+app.post(`/users`, async (req, res) => {
+  try {
+    const user = await UserModel.create(req.body);
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
 const port = 8080;
-
 app.listen(port, () => {
   console.log(`Rodando na porta: ${port}!`);
 });
--- src/database/connection.js
+++ src/database/connection.js
@@ -1,14 +1,12 @@
 const mongoose = require(`mongoose`);
 
 const connect = async () => {
-  await mongoose.connect(
-    `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cursonodejs.knjs32o.mongodb.net/?appName=CursoNodeJS`,
-    (error) => {
-      if (error) return console.log(`Error while connecting to DB: ${error}`);
-
-      return console.log(`Succesfully Connected to DB.`);
-    },
-  );
+  await mongoose
+    .connect(
+      `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cursonodejs.knjs32o.mongodb.net/?appName=CursoNodeJS`,
+    )
+    .then(console.log(`Succesfully Connected to DB.`))
+    .catch((error) => console.log(`Error while connecting to DB: ${error}`));
 };
 
 module.exports = connect;
